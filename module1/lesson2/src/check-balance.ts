5323de8e21cd3dd9d490c2056b6a924c47608055
elec7rik <vickyvirus258@gmail.com>
2025-05-06 02:04:33 +0530

    Add invalid pubkey check and mainnet query

--- module1/lesson2/src/check-balance.ts
+++ module1/lesson2/src/check-balance.ts
@@ -1,5 +1,5 @@
 import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
-const connection = new Connection(clusterApiUrl("devnet"));
+const connection = new Connection(clusterApiUrl("mainnet-beta"));
 
 // with public key supplied as an argument
 const suppliedPublicKey = process.argv[2];
@@ -7,6 +7,9 @@ if (!suppliedPublicKey) {
     throw new Error("❌ Provide a public key to check the balance of");
 }
 const publicKey = new PublicKey(suppliedPublicKey);
+if (!PublicKey.isOnCurve(publicKey.toBytes())) {
+    throw new Error("❌ Invalid public key")
+}
 const balance = await connection.getBalance(publicKey);
 console.log(`Sol balance at ${suppliedPublicKey} is ${balance/LAMPORTS_PER_SOL}`);
 console.log(`✅ Finished`);
